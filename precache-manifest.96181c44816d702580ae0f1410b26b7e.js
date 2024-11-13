4fbe89d5c02e7310c469ecc3a2887fb6350521fc
Amo C <amochen429@qq.com>
2024-11-13 11:06:12 +1000

    Updates

---
+++ precache-manifest.96181c44816d702580ae0f1410b26b7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e1424560d9e5f0495c6bf4cb1d2990",
+    "url": "/Amo2023/portfolio.git/index.html"
+  },
+  {
+    "revision": "a933af74b30491bdfaec",
+    "url": "/Amo2023/portfolio.git/static/css/main.d80a48ce.chunk.css"
+  },
+  {
+    "revision": "07e84b5c83a3a0624ceb",
+    "url": "/Amo2023/portfolio.git/static/js/2.e7146c92.chunk.js"
+  },
+  {
+    "revision": "a933af74b30491bdfaec",
+    "url": "/Amo2023/portfolio.git/static/js/main.3446d854.chunk.js"
+  },
+  {
+    "revision": "a33d6a2394a37b9caaa6",
+    "url": "/Amo2023/portfolio.git/static/js/runtime-main.c966a9a0.js"
+  },
+  {
+    "revision": "295c57db1b5514b90ad1945caeacfc91",
+    "url": "/Amo2023/portfolio.git/static/media/HighStreetGym.295c57db.png"
+  },
+  {
+    "revision": "c81fc847c18cba30a99e0349e4654bbb",
+    "url": "/Amo2023/portfolio.git/static/media/bridge_600.c81fc847.jpg"
+  },
+  {
+    "revision": "40e108cd38ea5bc0861b3cdf4e1dabb5",
+    "url": "/Amo2023/portfolio.git/static/media/weatherRecordingAPI.40e108cd.png"
+  }
+]);
