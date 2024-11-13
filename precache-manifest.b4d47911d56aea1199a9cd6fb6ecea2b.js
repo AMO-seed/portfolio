627c7850fe1b92c3542e7f07a22451c6e7919de1
Amo C <amochen429@qq.com>
2024-11-13 11:11:32 +1000

    Updates

---
+++ precache-manifest.b4d47911d56aea1199a9cd6fb6ecea2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69f5d926a449fa3d6e01a2285018fd2",
+    "url": "/Amo2023/portfolio/index.html"
+  },
+  {
+    "revision": "3cbc2e34b80e2df87e4a",
+    "url": "/Amo2023/portfolio/static/css/main.79330d01.chunk.css"
+  },
+  {
+    "revision": "07e84b5c83a3a0624ceb",
+    "url": "/Amo2023/portfolio/static/js/2.e7146c92.chunk.js"
+  },
+  {
+    "revision": "3cbc2e34b80e2df87e4a",
+    "url": "/Amo2023/portfolio/static/js/main.10945282.chunk.js"
+  },
+  {
+    "revision": "76debf15dc8be4d54a5e",
+    "url": "/Amo2023/portfolio/static/js/runtime-main.8ac3b217.js"
+  },
+  {
+    "revision": "295c57db1b5514b90ad1945caeacfc91",
+    "url": "/Amo2023/portfolio/static/media/HighStreetGym.295c57db.png"
+  },
+  {
+    "revision": "c81fc847c18cba30a99e0349e4654bbb",
+    "url": "/Amo2023/portfolio/static/media/bridge_600.c81fc847.jpg"
+  },
+  {
+    "revision": "40e108cd38ea5bc0861b3cdf4e1dabb5",
+    "url": "/Amo2023/portfolio/static/media/weatherRecordingAPI.40e108cd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Amo2023/portfolio.git/precache-manifest.96181c44816d702580ae0f1410b26b7e.js"
+  "/Amo2023/portfolio/precache-manifest.b4d47911d56aea1199a9cd6fb6ecea2b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Amo2023/portfolio.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Amo2023/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
